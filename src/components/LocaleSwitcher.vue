c3a7810e4240e1b67b56aef9d9a1690320188e70
Pyruz <pyruz.janbaaz@gmail.com>
2022-02-22 10:51:30 +0400

    - add flags to header

--- src/components/LocaleSwitcher.vue
+++ src/components/LocaleSwitcher.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <ul>
+    <ul class="flags">
       <li class="lang-link" v-for="locale in supportedLocales" :key="locale" @click="switchLocale(locale)">
-        {{ $t('menu.' + locale) }}
+        <img :src="require(`@/assets/images/${locale}.png`)">
       </li>
     </ul>
   </div>
@@ -21,7 +21,7 @@ export default {
   methods: {
     switchLocale(locale) {
       if (this.$i18n.locale !== locale) {
-        const to = this.$router.resolve({ params: {locale} })
+        const to = this.$router.resolve({params: {locale}})
         return Trans.changeLocale(locale).then(() => {
           this.$router.push(to.location)
         })
